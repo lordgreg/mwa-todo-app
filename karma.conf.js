6b46594f0399d819bf04e8fb698504245daec8b7
Gregor Papez <g.papez@mwaysolutions.com>
2017-03-20 08:55:31 +0100

    Updating karma and protractor conf

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,14 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec', 'coverage'],
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      // type: 'text'
+      type: 'html',
+      dir: 'test/coverage/'
+    },
 
     // web server port
     port: 9876,
--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
 // An example configuration file.
 exports.config = {
   // The address of a running selenium server.
@@ -7,13 +9,48 @@ exports.config = {
   //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
 
   // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
+  maxSessions: 1,
+  multiCapabilities: [
+    // tablet
+    {
+      browserName: 'chrome',
+      chromeOptions: {
+        'args': [
+          '--window-size=768,1024'
+        ]
+      }
+    },
+    // phone
+    {
+      browserName: 'chrome',
+      chromeOptions: {
+        'args': [
+          '--window-size=375,667'
+        ]
+      }
+    },
+  ],
 
   baseUrl: 'http://localhost:3000',
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
   specs: ['test/protractor/**/*.js'],
+
+  jasmineNodeOpts: {
+    showColors: true,
+    silent: true,
+    // defaultTimeoutInterval: 360000,
+    print: function () {
+    }
+  },
+
+  onPrepare: function () {
+    /*globals jasmine*/
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: true
+      }
+    }));
+  }
 };
